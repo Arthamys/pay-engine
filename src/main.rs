36e75cdd67b26b76df92abe1aeb92b156e4690bb
san <san@alien.parts>
2021-03-04 00:49:50 +0100

    Automatically run against random input
    If we run the payment engine without arguments, it now generates
    a list of 1 million random transactions, and applies them to the
    engine.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use parser::Parser;
 use pay_engine::*;
+use transaction::{utils::RandomTransactions, Transaction};
+
 use simple_logger::SimpleLogger;
 
 fn main() -> Result<()> {
@@ -11,32 +13,38 @@ fn main() -> Result<()> {
 
     // skip program name
     let mut args = std::env::args();
-    let prog_name = args.next().expect("USAGE: cargo run -- input.csv");
+    let _prog_name = args
+        .next()
+        .expect("USAGE: cargo run #Runs with random number of tx");
 
-    if let Some(filepath) = args.next() {
-        run_engine(filepath).map_err(|e| {
-            eprintln!("Could not run engine ({})", e);
-            error::Error::DeserializeError
-        })?;
-    } else {
-        println!("USAGE: {} intput.csv > output.csv", prog_name);
-    }
+    run_engine(args.next()).map_err(|e| {
+        eprintln!("Could not run engine ({})", e);
+        error::Error::DeserializeError
+    })?;
     Ok(())
 }
 
-pub fn run_engine(filepath: String) -> Result<()> {
-    let parser = match Parser::new(&filepath) {
-        Ok(p) => p,
-        Err(err) => {
-            eprintln!("{}", err);
-            return Ok(());
-        }
-    };
-
-    let (wallets, _) = engine::run(&mut parser.into_iter());
+pub fn run_engine(filepath: Option<String>) -> Result<()> {
+    let mut transactions = get_transaction_stream(filepath)?;
+    let (wallets, _) = engine::run(&mut transactions);
 
     wallets.print_balances().map_err(|e| {
         eprintln!("Could not serialize wallet balances ({})", e);
         error::Error::SerializeError
     })
+}
+
+fn get_transaction_stream(
+    filepath: Option<String>,
+) -> Result<Box<dyn Iterator<Item = Transaction>>> {
+    if let Some(file) = filepath {
+        match Parser::new(&file) {
+            Ok(p) => return Ok(Box::new(p)),
+            Err(err) => {
+                eprintln!("{}", err);
+                return Err(err);
+            }
+        }
+    }
+    return Ok(Box::new(RandomTransactions::new().take(10_000_000)));
 }
---
+++ tests/integration_test.rs
@@ -0,0 +1,23 @@
+use pay_engine::{engine, transaction::utils::RandomTransactions};
+use std::iter::Iterator;
+
+#[test]
+#[ignore]
+fn exec_100_million_tx() {
+    let tx_gen = RandomTransactions::new();
+    engine::run(
+        &mut tx_gen
+            .into_iter()
+            .take(/*u32::max_value()*/ 100_000_000 as usize),
+    );
+}
+
+#[test]
+fn exec_1_million_tx() {
+    let tx_gen = RandomTransactions::new();
+    engine::run(
+        &mut tx_gen
+            .into_iter()
+            .take(/*u32::max_value()*/ 1_000_000 as usize),
+    );
+}
